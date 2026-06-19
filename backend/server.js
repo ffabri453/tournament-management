a37af99e897147cc8a1dc30650089e9920bd5ca4
Julián Agulló <agullojulian2005@hotmail.com>
2026-06-19 17:03:48 -0300

    Corrections

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
+const pool = require("./src/config/database");
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Backend funcionando");
+    res.send("Backend is working");
 });
 
 app.listen(3000, () => {
-    console.log("Servidor corriendo en puerto 3000");
+    console.log("Server is running on port 3000");
 });
-
