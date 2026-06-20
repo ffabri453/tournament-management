e63b7617d4c08efc44e2c341c7348902d361d001
tizibenetti5 <tiziibenetti5@gmail.com>
2026-06-20 01:34:18 -0300

    Enable JSON request body parsing

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
+
 import pool from './src/config/db';
 
 const app = express();
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 // Ruta base
